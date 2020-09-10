91883cd0d203aeb6c0923af2556573545616a26a
dandelionadia <nadia.ridko@gmail.com>
2020-09-10 15:26:12 +0200

    migrates from Material UI to Chakra UI

--- pages/login.js
+++ pages/login.js
@@ -1,27 +1,16 @@
 import { useContext, useState, useEffect } from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import { CardHeader } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import { GoogleLogin } from "react-google-login";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
+import { Box, Heading, Text } from "@chakra-ui/core";
 
 import { AuthContext } from "../context/AuthContext";
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 350,
-    margin: "2rem auto",
-  },
-});
 
 export default function Login() {
   const { accessToken, setAccessToken } = useContext(AuthContext);
   const [errorMessage, setErrorMessage] = useState("");
 
   const router = useRouter();
-  const classes = useStyles();
 
   const handleAuthSuccess = (response) => {
     setAccessToken(response.accessToken);
@@ -45,22 +34,33 @@ export default function Login() {
 
   return (
     <div>
-      <Card className={classes.root}>
-        <CardHeader
-          title="Login"
-          subheader="Use your Google account to log in."
+      <Box
+        d="flex"
+        flexDirection="column"
+        p={4}
+        alignItems="center"
+        shadow="md"
+        borderWidth="1px"
+        width="500px"
+        maxWidth="100%"
+        margin="auto"
+        mt={6}
+      >
+        <Heading as="h2" size="xl">
+          Login
+        </Heading>
+        <Text fontSize="lg" p={4}>
+          Use your Google account to log in.
+        </Text>
+        <GoogleLogin
+          clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+          scope="profile email https://www.googleapis.com/auth/youtube.readonly"
+          buttonText="Login"
+          onSuccess={handleAuthSuccess}
+          onFailure={handleAuthFailure}
         />
-        <CardContent>
-          <GoogleLogin
-            clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
-            scope="profile email https://www.googleapis.com/auth/youtube.readonly"
-            buttonText="Login"
-            onSuccess={handleAuthSuccess}
-            onFailure={handleAuthFailure}
-          />
-          {errorMessage && <p>{errorMessage}</p>}
-        </CardContent>
-      </Card>
+        {errorMessage && <p>{errorMessage}</p>}
+      </Box>
     </div>
   );
 }
